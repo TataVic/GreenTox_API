55a9c8bc49854941ab5641b8be5434797e16d99e
Bianca Netto <biancanettoc@gmail.com>
2024-06-11 17:28:35 -0300

    up

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+global $config;
+
+$config = array();
+define("BASE_URL","http://localhost/GreenTox_API");
+$config['dbname'] = 'agrotox';
+$config['host']   = 'localhost';
+$config['dbuser'] = 'root';
+$config['dbpass'] = '';
