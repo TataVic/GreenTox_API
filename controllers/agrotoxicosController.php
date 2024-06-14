e7acd950b2fa5c9ab97700635868fb342e3f5a53
Bianca Netto <biancanettoc@gmail.com>
2024-06-14 17:06:12 -0300

    metodos

--- controllers/agrotoxicosController.php
+++ controllers/agrotoxicosController.php
@@ -7,15 +7,41 @@ class agrotoxicosController extends controller{
 		parent::__construct();
 		$this->dados = array();
 	}
-
 	public function index(){
-
 		//$api = new Api(); // quando tiveer o token instanciar
-
 		$agrotoxicos = new Agrotoxicos();
 		$lista = $agrotoxicos->getAll();
-
 		output_header(true,'todos os produtos',$lista);
 	}
+	
+		public function get($id) {
+			// Lógica para retornar um agrotóxico pelo ID
+		}
+	
+		public function create() {
+			// Lógica para criar um novo agrotóxico
+		}
+	
+		public function update($id) {
+			// Lógica para atualizar um agrotóxico pelo ID
+		}
+	
+		public function delete($id) {
+			// Lógica para deletar um agrotóxico pelo ID
+		}
+	}
+	
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
